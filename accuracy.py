3f5c48049f2d663cb528c71101ac2fea645f1325
pdchyy <41654200+pdchyy@users.noreply.github.com>
2025-02-17 11:28:38 +1300

    Update accuracy.py

--- accuracy.py
+++ accuracy.py
@@ -94,10 +94,9 @@ def validate_1(model, validation_data, input_height=360, input_width=640, output
         imgs = get_input(input_height, input_width, path, path_prev, path_preprev) # combine 3 frames 
         
         prediction = model.predict(np.array([imgs]), verbose=0)[0]
-        # print("prediction.shape:", prediction.shape)
+    
         x_pred, y_pred = heatMap_1(prediction, input_height, input_width, output_height, output_width)
-        # x_pred, y_pred = binary_heatMap(prediction)
-        
+     
         vis = int(vis)
         
         if x_pred:
