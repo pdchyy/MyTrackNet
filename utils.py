143d090210d9a979d9f102f1dc16195dedb701d2
pdchyy <41654200+pdchyy@users.noreply.github.com>
2025-02-25 12:08:08 +1300

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,9 +7,9 @@ import keras.backend as K
 
 def heatMap(prediction, n_classes, model_height, model_width, output_height, output_width):
     """ Use the cv2.threshold and HoughCircles to get the ball centre"""
-    # start_time = time.time()
+
     prediction = prediction.reshape((model_height, model_width, n_classes)).argmax(axis=2) # loss= sparceCategoricalCrossEntropy
-    print("prediction.shape:", prediction.shape)
+    
     prediction = prediction.astype(np.uint8)
 
     feature_map = cv2.resize(prediction, (output_width, output_height))
@@ -22,38 +22,32 @@ def heatMap(prediction, n_classes, model_height, model_width, output_height, out
             x = int(circles[0][0][0])
             y = int(circles[0][0][1])
     
-    # run_time = time.time() - start_time
-    # print( "The heatMap running time is ", run_time)
     return x, y
 
 ## This is for WBCE_loss
 def heatMap_1(prediction, model_height, model_width, output_height, output_width):
     """ Use the cv2.threshold and HoughCircles to get the ball centre"""
-    # start_time = time.time()
-    print("prediction_0.shape:", prediction.shape)
-    # prediction = prediction.reshape((model_height, model_width, 3)).argmax(axis=2)
-    prediction = prediction.reshape((model_height, model_width)) # the final index is 1 for 1-frame-out and 3 for 3-frames-out.
-    print("prediction.shape:", prediction.shape)
-    # prediction = prediction.reshape((model_height, model_width)) # loss= WBCE_loss
+  
+    prediction = prediction > 0.5
+    prediction = prediction.astype('float32')
+    prediction = prediction * 255
+    prediction = prediction.astype('uint8')
+    prediction = prediction.reshape((model_height, model_width)) # loss= WBCE_loss
     prediction = prediction.astype(np.uint8)
 
     feature_map = cv2.resize(prediction, (output_width, output_height))
-    # feature_map = cv2.resize(prediction, (model_width, model_height))
+   
     ret, feature = cv2.threshold(feature_map, 127, 255, cv2.THRESH_BINARY)
-    # ret, feature = cv2.threshold(prediction, 127, 255, cv2.THRESH_BINARY)
 
-    print("featuer.shape:", feature.shape)
     circles = cv2.HoughCircles(feature, cv2.HOUGH_GRADIENT, dp=1,
                                minDist=1, param1=50, param2=2, minRadius=2, maxRadius=7)
     print("cirles:", circles)
     x, y = None, None
     if circles is not None:
         if len(circles) == 1:
-            x = int(circles[0][0][0])
-            y = int(circles[0][0][1])
-    
-    # run_time = time.time() - start_time
-    # print( "The heatMap running time is ", run_time)
+            x = float(circles[0][0][0])
+            y = float(circles[0][0][1])
+        
     return x, y
 
 
@@ -113,17 +107,25 @@ def get_output(height, width, path_gt):
     img = np.reshape(img, (width* height))
     return img
 
-def generate_binary_heatmap(cx, cy, r, mag=1):
-        if cx < 0 or cy < 0:
-            return np.zeros((1, self.height, self.width))
+def generate_binary_heatmap(cx, cy, r, mag):
+        height = 360
+        width = 640
         
-        x, y = np.meshgrid(np.linspace(1, self.width, self.width), np.linspace(1, self.height, self.height))
+        if vis > 0:
+            cx = int(float(cx.strip()))
+            cy = int(float(cy.strip()))
+
+        else:
+            return np.zeros((1, height, width))
+        
+        x, y = np.meshgrid(np.linspace(1, width, width), np.linspace(1, height, height))
         heatmap = ((y - (cy + 1))**2) + ((x - (cx + 1))**2) # ?
         heatmap[heatmap <= r**2] = 1
         heatmap[heatmap > r**2] = 0
         y = heatmap*mag
-        y = np.reshape(y, (1, self.height, self.width))
+        y = np.reshape(y, (1, height, width))
         return y
+
 
 def WBCE_loss(y_true, y_pred): 
     """" Weighted binary crossentropy loss function"""
@@ -138,22 +140,17 @@ def WBCE_loss(y_true, y_pred):
     return ops.mean(loss)
 
 
-
-def WBCELoss(y_pred, y, reduce=True):
-    """ Weighted Binary Cross Entropy loss function defined in TrackNetV2 paper.
-
-        Args:
-            y_pred (torch.Tensor): Predicted values with shape (N, 1, H, W)
-            y (torch.Tensor): Ground truth values with shape (N, 1, H, W)
-            reduce (bool): Whether to reduce the loss to a single value or not
-
-        Returns:
-            (torch.Tensor): Loss value with shape (1,) if reduce, else (N, 1)
-    """
+def BCE_loss(y_true, y_pred): 
+    """" binary crossentropy loss function, it can not be used for for imbalanced binary heatMap"""
     
-    loss = (-1)*(torch.square(1 - y_pred) * y * torch.log(torch.clamp(y_pred, 1e-7, 1))\
-            + torch.square(y_pred) * (1 - y) * torch.log(torch.clamp(1 - y_pred, 1e-7, 1)))
-    if reduce:
-        return torch.mean(loss)
+    if y_pred is None:
+        # y_pred = np.array(0.0, dtype=np.float32)
+        y_pred = tf.constant([0.0], tf.float32)
     else:
-        return torch.mean(torch.flatten(loss, start_dim=1), 1)
+        tf.cast(y_pred, tf.float32)
+
+    loss = (-1)* y_true * tf.math.log(tf.clip_by_value(y_pred, 1e-07, 1)) + (1 - y_true) * tf.math.log(tf.clip_by_value(1 - y_pred, 1e-07, 1)) # Binary CrossEntropy loss
+    # loss = (-1)* (y_true * ops.log(ops.clip(y_pred, 1e-7, 1)) +  (1 - y_true) * ops.log(ops.clip(1 - y_pred, 1e-7, 1))) # Binary CrossEntropy loss
+    return ops.mean(loss)
+
+
