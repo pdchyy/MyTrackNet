8009d2bcd7264b979d62339cb5057dea9cc711ef
pdchyy <41654200+pdchyy@users.noreply.github.com>
2025-05-02 09:02:28 +1200

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,17 @@
-from model import build_track_net
+## This is traning for original TrackNet model
+
+from model import TrackNet, U_net
 from datasets import TrackNetDataset
 from custom_callback import ValidationCallback
 import argparse, os
 from pathlib import Path
 import tensorflow as tf
 from keras import optimizers
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
-from tensorflow.keras.models import load_model
-
+# from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+# from tensorflow.keras.models import load_model
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.models import load_model
+from utils import WBCE_loss
 
 if __name__ == '__main__':
     root = Path(__file__).parent
@@ -16,10 +20,10 @@ if __name__ == '__main__':
     parser.add_argument("--n_classes", type=int, default=256)
     parser.add_argument("--input_height", type=int, default=360)
     parser.add_argument("--input_width", type=int, default=640)
-    parser.add_argument("--epochs", type=int, default=10) #default=500)
-    parser.add_argument("--batch_size", type=int, default=2)
+    parser.add_argument("--epochs", type=int, default=400)# Based on the validation loss, and get the acceptable result at epochs=99 and 50. 500 epochs can cause overfitting
+    parser.add_argument("--batch_size", type=int, default=2) # 2 change to ?, the batch_size increasing will cause the processing time increasing
     parser.add_argument("--load_model_status", type=str, default=False)
-    parser.add_argument("--steps_per_epoch", type=int, default=20) #default=200)
+    parser.add_argument("--steps_per_epoch", type=int, default=200) # 200 change to 300, which means more films(batches) are trained.
 
     args = parser.parse_args()
     batch_size = args.batch_size
@@ -38,23 +42,33 @@ if __name__ == '__main__':
     else:
       # model = TrackNet(n_classes, input_height=input_height, input_width=input_width)
       model = U_net(n_classes, input_height=input_height, input_width=input_width)
-      model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(), optimizer=optimizer_name, metrics=['accuracy'])
 
+      model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(), optimizer=optimizer_name, metrics=['accuracy'])
+      
     train_dataset = TrackNetDataset(input_height, input_width, batch_size)
     # stop_early = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10)
-    validation_callback = ValidationCallback()
+    validation_callback = ValidationCallback() # This was created by the author for viewing the training results in figures at media
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="logs",
                                                           histogram_freq=1,
                                                           profile_batch='20,180')
-    model_checkpoint = ModelCheckpoint(f'{save_model_path}/tracknet.keras', save_weights_only=False,
-                                       save_freq=10000,
+    model_checkpoint = ModelCheckpoint(f'{save_model_path}/tracknet.keras', save_weights_only=False, # model was saved here
+                                      #  save_freq=10000, # using so high value means not saving model, save_freq means the number of batches
+                                       save_freq="epoch", # save the model per epoch
                                        monitor="loss",
                                        mode='min',
                                        verbose=1)
 
     model.fit(train_dataset, epochs=epochs, verbose=1, steps_per_epoch=steps_per_epoch,
-              max_queue_size=10,
-              workers=2,
-              use_multiprocessing=True,
+              # max_queue_size=10,
+              # workers=2,
+              # use_multiprocessing=True,
               callbacks=[model_checkpoint,validation_callback]
               )
+    
+    # model.save(f'{save_model_path}/tracknet.keras')
+
+# epochs=500
+# precision = 0.8462350457424349
+# recall = 0.9026083693000557
+# f1 = 0.9670285484519502
+# Validation loss: 0.0016903852240212477
